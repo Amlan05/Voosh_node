66f782ea06c4a57f972702e5f28f1be4d659a9de
Amlan05 <itzamlan05@gmail.com>
2024-02-13 14:00:47 +0530

    updt

--- Backend/Routes/UserRoutes.js
+++ Backend/Routes/UserRoutes.js
@@ -6,5 +6,4 @@ const userController = require('../Controller/UserController')
 userRouter
 .post("/add-user", userController.userRegister)
 .post('/login-user', userController.loginUser)
-.get('/', userController.getUsers)
 exports.userRouter = userRouter 
